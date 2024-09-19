1108e9de3088af56d92dfc97a0ea4ec1aa6bf329
Evan Linder <109065222+Evan-Linder@users.noreply.github.com>
2024-09-19 18:31:35 -0500

    New sprites/animations
    Added different sprites from the asset store to better fit the genre of the game. Also added some new idle and running animations

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -8,12 +8,20 @@ public class PlayerController : MonoBehaviour
 
     private PlayerControls playerControls; // input actions object
     private Vector2 movement; // store the player input
-    private Rigidbody2D rb; // reference rb componet
+
+    // references to our components
+    private Rigidbody2D rb; 
+    private Animator myAnimator; 
+    private SpriteRenderer mySpriteRender; 
 
     private void Awake()
     {
         playerControls = new PlayerControls(); // player controls object
-        rb = GetComponent<Rigidbody2D>(); // get rb component attached to player 
+
+        // attach all the componets to our variables.
+        rb = GetComponent<Rigidbody2D>(); 
+        myAnimator = GetComponent<Animator>(); 
+        mySpriteRender = GetComponent<SpriteRenderer>(); 
     }
     private void OnEnable()
     {
@@ -25,15 +33,35 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        ChangePlayerDirection();
         Move();  
     }
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>(); // read input from move action
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
     private void Move()
     {
         // move the player based on the movement input (current position + movement * move speed)
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime)); // use delta fixed time to avoid frame rate issues.
+    }
+
+    private void ChangePlayerDirection()
+    {
+        Vector3 mousePos = Input.mousePosition; // set the current position of the mouse on the screen
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position); // convert users position to screen space cord. (used in 2D systems)
+
+        // check if the mouse is to the left of the player
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true; // flip player to face the left
+        }
+        else
+        {
+            mySpriteRender.flipX = false; // flip player to face the right
+        }
     }
 }
